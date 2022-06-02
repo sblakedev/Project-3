c2a52e0f6c6c8997b23b3ef1b652a1a36a7607cd
Suzanne Blake <suzanne.blake22@gmail.com>
2022-06-02 19:29:00 +0000

    Create and check Welcome function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,13 +13,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman')
 
-words = SHEET.worksheet('words')
+def welcome():
+    """
+    Welcomes user to the game.
+    Explains how to play the game. 
+    Asks user to enter their name
+    """
+    print("Welcome to Hangman!")
+    print("To play, guess the letters in a random 5 letter word.")
+    print("You will have 7 lives.")
+    print("If your letter is correct, your points will increase by one.")
+    print("If your letter is incorrect, you will lose a life.")
+    
+    name_str = input("Please enter your name: ")
+    print(f"Hello {name_str}. Let's play Hangman!")
+    
 
-data = words.get_all_values()
+welcome()
 
-print(data)
-
-start_game
+"""start_game
 
 random_word_selector
 
@@ -32,5 +45,5 @@ lose_life
 
 guess_all_letters
 
-play_again
+play_again"""
 
