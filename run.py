1bcc24d9602a2cfc8445f8d2736a84bf765e81ae
Suzanne Blake <suzanne.blake22@gmail.com>
2022-06-10 15:51:09 +0000

    Add scope

--- run.py
+++ run.py
@@ -4,3 +4,14 @@ from google.oauth2.service_account import Credentials
 import random
 from IPython.display import clear_output
 import string
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman')
